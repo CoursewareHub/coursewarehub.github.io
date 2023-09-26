8d67e515f5ef112997c8a04b5dd5e65235aacf0d
n08u <n08u@users.noreply.github.com>
2023-09-26 09:44:52 +0000

    jekyll build from Action b536c59d835c4f84ea1351be4f6eaf641ff078ca

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "CoursewareHub利用事例 - 群馬大学 井上先生",
+        "excerpt":"CoursewareHubの利用事例を国立情報学研究所 学術基盤オープンフォーラム2021にて 群馬大学・数理データ科学教育研究センター 井上先生にCoursewareHubの利用事例をご紹介いただきました！   資料は以下のリンクからご参照いただけます。     ","categories": [],
+        "tags": ["利用例","群馬大学"],
+        "url": "/2021/08/10/gunma-u.html",
+        "teaser": null
+      },{
+        "title": "CoursewareHub構築手順Notebookを公開しました",
+        "excerpt":"Jupyter Notebookベースの「実行可能な」手順書形式による構築手順の配布を開始しました。   AWS上に作業用のOperationHubと、CoursewareHubを構築する手順となります。   この手順を参考に、オンプレミス環境など、他の環境への配備に応用することもできます。詳細は以下をご参照ください:   CoursewareHubの構築notebook      本手順は、2022年度 情報処理技術セミナー(クラウド編)でもご紹介をする予定です:     情報処理技術セミナー（クラウド編） 令和4年度　テーマ：CoursewareHub – Jupyter Notebook による講義・演習環境    ","categories": [],
+        "tags": ["Notebook"],
+        "url": "/2022/05/20/deploy.html",
+        "teaser": null
+      },{
+        "title": "2022年度 情報処理技術セミナー(クラウド編)にてCoursewareHubを体験いただきました",
+        "excerpt":"2022年度 情報処理技術セミナー(クラウド編)にて、受講生の皆さまにCoursewareHubを体験いただきました。   以下のようなカリキュラムで、CoursewareHubで学生がどんな体験をできるのか、環境の構築にはどのような作業をする必要があるのかをご体験いただきました。      10:30−12:00（90分）\t【実習１】CoursewareHubの機能を使ってみよう   13:30−15:00（90分）\t【実習２】CoursewareHubを構築してみよう（Ⅰ）   15:15−16:45（90分）\t【実習３】CoursewareHubを構築してみよう（Ⅱ）   情報処理技術セミナーは来年度も実施する予定です。もしご興味がある方は 情報処理技術セミナー（クラウド編） のウェブサイトを定期的にご確認ください。  ","categories": [],
+        "tags": ["event"],
+        "url": "/2022/08/05/seminar.html",
+        "teaser": null
+      },{
+        "title": "AXIES2022年次大会にてCoursewareHubのご紹介をします",
+        "excerpt":"AXIES2022年次大会にて、CoursewareHubの開発状況について国立情報学研究所 政谷よりご紹介します。   詳細は OSS企画セッション1 Jupyter等の環境を用いたプログラミング教育支援の現状 をご覧ください。   「CoursewareHub: Jupyter Notebookによる講義・演習環境」と題して、Jupyter Notebookを用いた講義・演習を運営するための環境CoursewareHub、国立情報学研究所で独自に開発・拡張している機能についてご紹介する予定です。  ","categories": [],
+        "tags": ["event"],
+        "url": "/2022/12/12/axies.html",
+        "teaser": null
+      }]
